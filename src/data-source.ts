56017def04b610d2e5c1aa6855cf8521d47d4f45
Mark Talbot <markjtalbot@gmail.com>
2026-04-12 21:22:11 -0400

    Wire up postgres

---
+++ src/data-source.ts
@@ -0,0 +1,9 @@
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  url: process.env.DATABASE_URL,
+  entities: ['src/**/*.entity{.ts,.js}'],
+  migrations: ['src/migrations/*{.ts,.js}'],
+});
